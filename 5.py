73ae57fb4a7eecd3dd370551aebc621ca979e89f
LuBlau <88215746+LuBlau@users.noreply.github.com>
2025-05-21 02:07:13 -0500

    Add files via upload

--- 5.py
+++ 5.py
@@ -1,27 +1,14 @@
 class Profesor:
-    """
-    Esta clase denominada Profesor es una superclase que representa un
-    profesor genérico.
-    """
-
     def imprimir(self):
         print("Es un profesor.")
 
-
 class ProfesorTitular(Profesor):
-    """
-    Esta clase denominada ProfesorTitular es una subclase de Profesor.
-    """
-
     def imprimir(self):
         print("Es un profesor titular.")
 
-
-# Clase de prueba que demuestra el polimorfismo
 def main():
-    profesor1 = ProfesorTitular()  # Se usa la clase hija pero se trata como clase padre
+    profesor1 = ProfesorTitular()
     profesor1.imprimir()
-
 
 if __name__ == "__main__":
     main()
